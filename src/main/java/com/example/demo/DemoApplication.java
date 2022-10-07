d0b172868c2f614140bb20ca18a3aae22feb4b57
venkabd <115159979+venkabd@users.noreply.github.com>
2022-10-07 12:50:37 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+		System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+    
+    System.out.println(first + " " + second);
 
-	@RequestMapping("/")
-	String sayHello() {
-		return "Hello venkat!!";
-	}
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
 }
